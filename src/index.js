c41848d6266a24269d1d338de84425ddd3f6a5a9
Ilya Krasilnikov <kravus27@gmail.com>
2021-07-22 18:18:19 +0300

    wdwdwdw

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-plusplus */
 import { ajax } from 'rxjs/ajax';
 import { map, catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
-import { interval } from 'rxjs';
+import { of, interval } from 'rxjs';
 
-let mainContainer = document.getElementById('main-container');
+const mainContainer = document.getElementById('main-container');
 
 function formatDateFromTimestamp(timestamp) {
   const d = new Date(timestamp * 1000);
@@ -15,55 +15,49 @@ function formatDateFromTimestamp(timestamp) {
   const YY = addZero(d.getFullYear() % 100);
   const HH = addZero(d.getHours());
   const mm = addZero(d.getMinutes());
-  const ss = addZero(d.getSeconds());
 
-  let dateString =  `${HH}:${mm}&nbsp;${DD}.${MM}.${YY}`;
-  let completeDate = dateString.replace('&nbsp;', ' ');
+  const dateString = `${HH}:${mm}&nbsp;${DD}.${MM}.${YY}`;
+  const completeDate = dateString.replace('&nbsp;', ' ');
   return completeDate;
 }
 
-
 function build(message) {
-
-  console.log(mainContainer.children[0]);
-
-  let messageContainer = document.createElement('div');
+  const messageContainer = document.createElement('div');
   messageContainer.classList.add('message-container');
 
-  let email = document.createElement('p');
+  const email = document.createElement('p');
   email.classList.add('email-data');
   email.textContent = message.from;
 
-  let subject = document.createElement('p');
+  const subject = document.createElement('p');
   subject.classList.add('subject-data');
-  let subjectStringLength = message.subject.length;
-  subjectStringLength > 15 ? subject.textContent = `${message.subject.slice(0,16)}...` : subject.textContent = message.subject;
+  const subjectStringLength = message.subject.length;
+  // eslint-disable-next-line no-unused-expressions
+  subjectStringLength > 15 ? subject.textContent = `${message.subject.slice(0, 16)}...` : subject.textContent = message.subject;
 
-  let date = document.createElement('p');
+  const date = document.createElement('p');
   date.classList.add('message-date');
   date.textContent = formatDateFromTimestamp(message.received);
-
 
   messageContainer.appendChild(email);
   messageContainer.appendChild(subject);
   messageContainer.appendChild(date);
 
-  let firstChildElement = mainContainer.children[0];
+  const firstChildElement = mainContainer.children[0];
   mainContainer.insertBefore(messageContainer, firstChildElement);
-  
 }
 
 const source = interval(1000);
-let sourceObs$ = source.subscribe(() => {
+const sourceObs$ = source.subscribe(() => {
   const obs$ = ajax.getJSON('https://rxjsserver.herokuapp.com/messages/unread').pipe(
     map((userResponse) => {
-      console.log('resp', userResponse);
-      let messages = userResponse.messages;
-      for (let i=0; i<messages.length; i++) {
+      const { messages } = userResponse;
+      for (let i = 0; i < messages.length; i++) {
         build(messages[i]);
       }
     }),
-    catchError(error => {
+    catchError((error) => {
+      // eslint-disable-next-line no-console
       console.log('error: ', error);
       return of(error);
     }),
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 test('should add two numbers', () => {
-    const received = 1 + 1;
-    const expected = 2;
-    expect(received).toBe(expected);
+  const received = 1 + 1;
+  const expected = 2;
+  expect(received).toBe(expected);
 });
